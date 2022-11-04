8ceaa255d30bbd91203e7a27dd7ebe3f796cefac
Joel Garcia <joel@ecx-labs.com>
2022-11-04 14:05:43 -0500

    Typescript project configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import auth from './api/auth';
+import dotenv from 'dotenv';
 
 class Server {
 	private app: express.Application;
 	private port: number;
 
 	constructor() {
+		// setting up environment variables
+		dotenv.config();
+		// setting up express
 		this.app = express();
 		// using defined routes
 		this.app.use(auth);
@@ -13,6 +17,7 @@ class Server {
 	}
 
 	public run(): void {
+		console.log("Trying to run on port " + this.port);
 		this.app.listen(this.port, () => {
 			console.log(`Server listening on port ${this.port}`);
 			// initializing databse
